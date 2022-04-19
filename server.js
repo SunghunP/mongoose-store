f2b5fa5ae365cecda3004072d438e8b0cd41ee3a
Sunghun Park <sunghun.josephp@gmail.com>
2022-04-18 23:58:46 -0700

    Create database connection

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 // Dependencies
 ////////////////////////////////////////
 const express = require('express');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const app = express();
 const PORT = process.env.PORT || 3001;
 const DATABASE_URL = process.env.DATABASE_URL;
@@ -10,11 +12,19 @@ const DATABASE_URL = process.env.DATABASE_URL;
 ////////////////////////////////////////
 // Middleware
 ////////////////////////////////////////
+app.use(express.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
+
+mongoose.connect(DATABASE_URL);
+
+const db = mongoose.connection;
+db.on('error', (err) => console.log(err.message, ". Mongo is not running"));
+db.on('connected', () => console.log('mongoose connected'));
+db.on('disconnected', () => console.log('mongoose disconnected'));
 
 ////////////////////////////////////////
 // Routes
 ////////////////////////////////////////
 app.listen(PORT, () => {
 	console.log(`You are listening on ${PORT}`);
-	console.log(`You can find us on ${DATABASE_URL}`);
 });
