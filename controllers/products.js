1df9fc28aae6326d91978b0c180b7d42c0e9f936
Sunghun Park <sunghun.josephp@gmail.com>
2022-04-24 11:13:55 -0700

    Layout routes order

--- controllers/products.js
+++ controllers/products.js
@@ -24,13 +24,35 @@ router.get('/seed', (req, res) => {
 	});
 });
 
-// Index - GET: 
+// Index - GET: Find all of products from database and render them in product_index.ejs 
 router.get('/', (req, res) => {
 	// Find all of the Products from the database
 	Product.find({}, (err, allProducts) => {
 		// Render index page for products
-		res.render('product_index.ejs', {products: allProducts})
+		res.render('product_index.ejs', {products: allProducts});
 	});
-})
+});
+
+// New - GET: render product_show.ejs 
+router.get('/new', (req, res) => {
+	res.send('ayoooofsjignsiognriugn')
+});
+
+// Create - POST: Post data of created product to the /products page 
+router.post('/');
+
+// Show - GET: Renders show page for product_show.ejs
+router.get('/:id', (req, res) => {
+	res.send('ayo')
+});
+
+// Edit - GET: Render show page for products_edit.ejs
+router.get('/:id/edit');
+
+// Update - PUT: Update the edited product
+router.put('/:id');
+
+// Destroy - DELETE: Delete the product
+router.delete('/:id');
 
 module.exports = router;
