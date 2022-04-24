e71599636526d1db764ea4160142e5ef1c2cf48a
Sunghun Park <sunghun.josephp@gmail.com>
2022-04-24 11:14:38 -0700

    Add morgan to dependencies

--- server.js
+++ server.js
@@ -4,14 +4,17 @@ require('dotenv').config();
 ////////////////////////////////////////
 const express = require('express');
 const methodOverride = require('method-override');
+const morgan = require('morgan') // HTTP status reader
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 ////////////////////////////////////////
 // Middleware
 ////////////////////////////////////////
-app.use(express.urlencoded({extended: false}));
-app.use(methodOverride('_method'));
+app.use(express.urlencoded({extended: false})); // parses url encoded bodies
+app.use(methodOverride('_method')); 
+app.use("/public", express.static('public')); // serve files from public statically 
+app.use(morgan("dev")); // log every HTTP request
 
 // Route manager for the product controller
 const productRouter = require('./controllers/products');
